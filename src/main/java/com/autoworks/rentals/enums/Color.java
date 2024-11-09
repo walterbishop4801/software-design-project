28c42f3dbc7151f626451c6f77f8f0c707eedd6c
Rohan Sikder <rohansikder4@gmail.com>
2024-11-09 20:58:36 +0000

    Added Color and VehicleState enums

---
+++ src/main/java/com/autoworks/rentals/enums/Color.java
@@ -0,0 +1,5 @@
+package com.autoworks.rentals.enums;
+
+public enum Color {
+    RED, BLUE, YELLOW, GREEN, ORANGE, BLACK;
+}
---
+++ src/main/java/com/autoworks/rentals/enums/VehicleState.java
@@ -0,0 +1,5 @@
+package com.autoworks.rentals.enums;
+
+public enum VehicleState {
+    AVAILABLE, RESERVED, DAMAGED, IN_MAINTENANCE;
+}
