e5a5664425d738266853cab2ceb5a7cccd22a04e
Shane Barden <shanebar@live.ie>
2024-11-15 00:00:32 +0000

    Create Mechanic.java

---
+++ src/main/java/com/ul/vrs/entity/account/Mechanic.java
@@ -0,0 +1,37 @@
+package com.ul.vrs.entity.account;
+
+import com.ul.vrs.entity.vehicle.Vehicle;
+
+public class Mechanic {
+    
+
+
+
+
+
+    public void assignVehicle(Vehicle v){
+        
+        
+    }
+
+    public void liberateVehicle(Vehicle v){
+        
+        
+    }
+
+    public void serviceVehicle(Vehicle v){
+        
+        
+    }
+
+    public void fixVehicle(Vehicle v){
+        
+        
+    }
+
+
+
+
+
+
+}
