c2941d0f46e48e9532f018fd747faed3d5c5d03b
Shane Barden <shanebar@live.ie>
2024-12-04 01:19:29 +0000

    Update VehicleFactory.java

--- src/main/java/com/ul/vrs/entity/vehicle/factory/VehicleFactory.java
+++ src/main/java/com/ul/vrs/entity/vehicle/factory/VehicleFactory.java
@@ -1,8 +1,8 @@
 package com.ul.vrs.entity.vehicle.factory;
 
-import com.ul.vrs.entity.Color;
+
 import com.ul.vrs.entity.vehicle.Vehicle;
-import com.ul.vrs.entity.vehicle.fuel.Fuel;
+
 
 
 public interface VehicleFactory {
