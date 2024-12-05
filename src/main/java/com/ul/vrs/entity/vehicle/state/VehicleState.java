72d4a47cbf21222c7a7a75de785b9250e0021743
David Parreño Barbuzano <losedavidpb@gmail.com>
2024-12-05 13:28:55 +0000

    fix(vehicle): fix jackson annotations of VehicleState

--- src/main/java/com/ul/vrs/entity/vehicle/state/VehicleState.java
+++ src/main/java/com/ul/vrs/entity/vehicle/state/VehicleState.java
@@ -1,7 +1,17 @@
 package com.ul.vrs.entity.vehicle.state;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.ul.vrs.entity.vehicle.Vehicle;
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
+
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = AvailableVehicleState.class, name = "Available"),
+    @JsonSubTypes.Type(value = DamagedVehicleState.class, name = "Damaged"),
+    @JsonSubTypes.Type(value = InMaintenanceVehicleState.class, name = "In Maintenance"),
+    @JsonSubTypes.Type(value = ReservedVehicleState.class, name = "Reserved")
+})
 public interface VehicleState {
     boolean check(Class<? extends VehicleState> className);
     void handleRequest(Vehicle vehicle);
